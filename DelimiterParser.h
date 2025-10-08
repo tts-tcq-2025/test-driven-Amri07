4a6f749f32b80aace117b6b691db9eb392b74467
Amri07 <128670136+Amri07@users.noreply.github.com>
2025-10-08 11:20:20 +0530

    Update DelimiterParser.h

--- DelimiterParser.h
+++ DelimiterParser.h
@@ -1,13 +1,17 @@
-#ifndef STRING_SPLITTER_H_
-#define STRING_SPLITTER_H_
+#ifndef DELIMITER_PARSER_H_
+#define DELIMITER_PARSER_H_
 
 #include <string>
 #include <vector>
 
-class StringSplitter {
- public:
-  static std::string extractDelimiter(const std::string& numbers, std::string& numStr);
-  static std::vector<std::string> split(const std::string& str, const std::string& delimiter);
+class DelimiterParser {
+public:
+    static std::string extractDelimiter(const std::string& numbers, std::string& numStr);
+    static std::vector<std::string> split(const std::string& str, const std::string& delimiter);
+
+private:
+    static std::string getToken(const std::string& str, size_t start, size_t pos);
+    static void addTokenIfNotEmpty(std::vector<std::string>& tokens, const std::string& token);
 };
 
-#endif  // STRING_SPLITTER_H_
+#endif  // DELIMITER_PARSER_H_

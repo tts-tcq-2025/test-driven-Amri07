fd854911b7c8039f4af289c0e4d7b7ee79e2a2e4
Amri07 <128670136+Amri07@users.noreply.github.com>
2025-10-09 11:04:45 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -9,7 +9,7 @@ class StringCalculator {
   int add(const std::string& numbers);
 
  private:
-  std::vector<int> parseNumbers(const std::string& numbers, std::string& delimiter);
+  std::vector<int> parseNumbers(const std::string& numbers, const std::string& delimiter);
   void validateNumbers(const std::vector<int>& numbers);
   int sumNumbers(const std::vector<int>& numbers);
   std::string join(const std::vector<int>& numbers, const std::string& delimiter);

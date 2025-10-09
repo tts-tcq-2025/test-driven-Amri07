f6163c922d11ad012216698a3255628455e5ac77
Amri07 <128670136+Amri07@users.noreply.github.com>
2025-10-09 14:05:40 +0530

    Update DelimiterParser.h

--- DelimiterParser.h
+++ DelimiterParser.h
@@ -1,28 +1,20 @@
 #ifndef DELIMITERPARSER_H_
 #define DELIMITERPARSER_H_
-
 #if defined(__cplusplus)
 #include <string>
 #include <vector>
-
 namespace calculator {
-
 class DelimiterParser {
  public:
   static std::string extractDelimiter(const std::string& numbers, std::string& numStr);
-  static std::vector<std::string> split(const std::string& str, 
-                                      const std::string& delimiter);
-
+  static std::vector<std::string> split(const std::string& str, const std::string& delimiter);
  private:
   DelimiterParser() = delete;
   static std::string getToken(const std::string& str, size_t start, size_t pos);
-  static void addTokenIfNotEmpty(std::vector<std::string>& tokens, 
-                               const std::string& token);
+  static void addTokenIfNotEmpty(std::vector<std::string>& tokens, const std::string& token);
 };
-
 }  // namespace calculator
 #else
 typedef struct DelimiterParser DelimiterParser;
 #endif
-
 #endif  // DELIMITERPARSER_H_

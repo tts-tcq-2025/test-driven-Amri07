66e7549867352e73688d116fa043af53027f2ed2
Venugopal K <venu.shastri@gmail.com>
2025-10-10 09:25:59 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,1 +1,11 @@
+#include <string>
 
+namespace calculator {
+class StringCalculator {
+ public:
+  StringCalculator() = default;
+  ~StringCalculator() = default;
+  StringCalculator(const StringCalculator&) = delete;
+  StringCalculator& operator=(const StringCalculator&) = delete;
+  int add(const std::string& numbers);
+};

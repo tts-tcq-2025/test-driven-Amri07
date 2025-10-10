e2b8a01f12e842f88d1d6058473b7f7c939a4f7c
Venugopal K <venu.shastri@gmail.com>
2025-10-10 09:45:40 +0530

    Bug Fixed

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,4 +1,5 @@
 #include "StringCalculator.h"
+#include <regex>
 
 
 namespace calculator {
@@ -8,6 +9,16 @@ int StringCalculator::add(const std::string& numbers) {
   if (numbers.empty()) {
     return 0;
   }
+    std::regex number_pattern("\\d+");
+    std::sregex_iterator begin(input.begin(), input.end(), number_pattern);
+    std::sregex_iterator end;
+
+    int count = std::distance(begin, end);
+
+    if (count == 1) {
+        int number = std::stoi(begin->str());
+        return number;
+    }
   throw "Not Implemented Exception";
 }
 }
